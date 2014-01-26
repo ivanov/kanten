963acea4370d5d785250a82704a43605e8f95327
Paul Ivanov <pi@berkeley.edu>
2014-01-25 22:44:31 -0800

    ok, getting closer now

--- kanten.py
+++ kanten.py
@@ -1,4 +1,6 @@
 import urwid
+import IPython
+#import pudb
 
 def show_or_exit(key):
     if key in ('q', 'Q'):
@@ -6,11 +8,12 @@ def show_or_exit(key):
     #txt.set_text(repr(key))
 
 fname = '/home/pi/fortunes/antoine_de_saintexupery'
+fname = '/home/pi/cur/das.txt'
 with open(fname) as f:
     text = f.read()
 
-txt = urwid.Text(text)
-txts = [urwid.Text(t) for t in text.split('%')]
+#txt = urwid.Text(text)
+txts = [urwid.Text(t) for t in text.split('\n')]
 pile  = urwid.Pile(txts)
 
 # what I really want here is a transpose of the GridFlow widget
@@ -20,16 +23,35 @@ pile  = urwid.Pile(txts)
 # [  cell[1]  cell[4] ]
 # [  cell[2]  cell[5] ]
 #
-#cols = urwid.Columns([pile, pile, pile])
+
+width=40
+height=60
+piles = []
+p = urwid.Pile([])
+for t in txts:
+    p.contents.append((t, p.options()))
+    if p.rows((width,)) > height:
+        piles.append(p)
+        p = urwid.Pile([])
+
+
+cols = urwid.Columns(piles, dividechars=10, min_width=width)
 
 # On second thought, this looks like too much internals. All we need to do is
 # maintain the window height, and how far down a pile goes. If it's too high,
 # go to the next column. The thing is, the GridFlow implementation makes all
 # boxes in a row the same height, and that's not what I'm looking for.
+#
 
-grid = urwid.GridFlow(txts, cell_width=20, h_sep=4, v_sep=0, align='left')
-fill = urwid.Filler(grid, 'top')
+def pile_height(p):
+    pass
+
+
+#grid = urwid.GridFlow(txts, cell_width=20, h_sep=4, v_sep=0, align='left')
+fill = urwid.Filler(cols, 'top')
 loop = urwid.MainLoop(fill, unhandled_input=show_or_exit)
 
 loop.run()
 
+IPython.embed()
+#pu.db
