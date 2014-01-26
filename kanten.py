45ab4cebc84d0ab1e5f7b692c1baefad095b44cb
Paul Ivanov <pi@berkeley.edu>
2014-01-26 12:26:17 -0800

    'g' and 'G' handling, no proper backfill yet

--- kanten.py
+++ kanten.py
@@ -1,24 +1,43 @@
 import urwid
 import IPython
 #import pudb
+DEBUG = True
 
+off_screen = []
 def show_or_exit(key):
     if key in ('q', 'Q'):
         raise urwid.ExitMainLoop()
     elif key in ('b', 'B'):
-        cols.contents.insert(0, cols.contents.pop())
+        #off_screen.append(cols.contents.pop())
+        if off_screen:
+            new_first = off_screen.pop()
+            cols.contents.insert(0, new_first)
+            cols.focus_position=0
+    elif key in ('g'):
+        # take it from the top
+        cols.contents = off_screen + cols.contents
+        cols.focus_position=0
+    elif key in ('G'):
+        # this is the end, my friends, the end, the end.
+        off_screen.extend(cols.contents)
+        # XXX: backfill here properly - fill the hole screen
+        cols.contents = [ off_screen.pop() ]
     else:
-        cols.contents.append(cols.contents.pop(0))
+        if cols.contents:
+            off_screen.append(cols.contents.pop(0))
     #txt.set_text(repr(key))
 
 fname = '/home/pi/fortunes/antoine_de_saintexupery'
 fname = '/home/pi/cur/das.txt'
+fname = '/home/pi/cur/eb.txt'
 with open(fname) as f:
     text = f.read()
 
 txt = urwid.Text(text)
 txts = [urwid.Text(t) for t in text.split('\n')]
-#txts = [urwid.LineBox(t) for t in txts]
+if DEBUG:
+    # my brain finds it easier to deal with boxes
+    txts = [urwid.LineBox(t) for t in txts]
 pile  = urwid.Pile(txts)
 
 # what I really want here is a transpose of the GridFlow widget
@@ -30,16 +49,20 @@ pile  = urwid.Pile(txts)
 #
 
 width=40
-height=40
+height=10
 piles = []
 p = urwid.Pile([])
 for t in txts:
-    p.contents.append((t, p.options()))
-    if p.rows((width,)) > height:
+    t_size = t.rows((width,))
+    p_size = p.rows((width,))
+    if t_size + p_size > height:
+        # need to cut t_size so that it fits within height
         #p = urwid.AttrMap(p, None, focus_map='reversed') 
         #p = urwid.Padding(p, width=('relative', 30))
         piles.append(p)
+        # start the next column
         p = urwid.Pile([])
+    p.contents.append((t, p.options()))
 
 #palette = [
 #    (None,  'light gray', 'white'),
@@ -70,5 +93,5 @@ loop = urwid.MainLoop(fill, unhandled_input=show_or_exit)
 
 loop.run()
 
-IPython.embed()
+#IPython.embed()
 #pu.db
