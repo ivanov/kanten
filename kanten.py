ebb8eb924f7148b77e3ce1e92664f1910441954c
Paul Ivanov <pi@berkeley.edu>
2014-01-25 23:55:31 -0800

    that's more like it! MVP.

--- kanten.py
+++ kanten.py
@@ -5,6 +5,10 @@ import IPython
 def show_or_exit(key):
     if key in ('q', 'Q'):
         raise urwid.ExitMainLoop()
+    elif key in ('b', 'B'):
+        cols.contents.insert(0, cols.contents.pop())
+    else:
+        cols.contents.append(cols.contents.pop(0))
     #txt.set_text(repr(key))
 
 fname = '/home/pi/fortunes/antoine_de_saintexupery'
@@ -14,7 +18,7 @@ with open(fname) as f:
 
 txt = urwid.Text(text)
 txts = [urwid.Text(t) for t in text.split('\n')]
-txts = [urwid.LineBox(t) for t in txts]
+#txts = [urwid.LineBox(t) for t in txts]
 pile  = urwid.Pile(txts)
 
 # what I really want here is a transpose of the GridFlow widget
@@ -32,34 +36,37 @@ p = urwid.Pile([])
 for t in txts:
     p.contents.append((t, p.options()))
     if p.rows((width,)) > height:
-        p = urwid.AttrMap(p, None, focus_map='reversed') 
+        #p = urwid.AttrMap(p, None, focus_map='reversed') 
         #p = urwid.Padding(p, width=('relative', 30))
         piles.append(p)
         p = urwid.Pile([])
 
-palette = [
-    (None,  'light gray', 'white'),
-    ('heading', 'black', 'light gray'),
-    ('line', 'black', 'light gray'),
-    ('options', 'dark gray', 'black'),
-    ('focus heading', 'white', 'dark red'),
-    ('focus line', 'black', 'dark red'),
-    ('focus options', 'black', 'light gray'),
-    ('selected', 'white', 'dark blue')]
+#palette = [
+#    (None,  'light gray', 'white'),
+#    ('heading', 'black', 'light gray'),
+#    ('line', 'black', 'light gray'),
+#    ('options', 'dark gray', 'black'),
+#    ('focus heading', 'white', 'dark red'),
+#    ('focus line', 'black', 'dark red'),
+#    ('focus options', 'black', 'light gray'),
+#    ('selected', 'white', 'dark blue')]
 
 #piles = urwid.ListBox(urwid.SimpleFocusListWalker(piles))
 #cols = piles
 #fill = cols
 cols = urwid.Columns(piles, focus_column=2,   dividechars=10, min_width=width)
-#cols.box_columns.extend(cols.widget_list)
 
-def pile_height(p):
-    pass
+# XXX: I need to subclass columns, and make it so the keypress function
+# "rolls" the piles under the hood, and re-renders all the widgets.
+#
+# self.contents.append(self.contents.pop(0))
+#
+#cols.box_columns.extend(cols.widget_list)
 
 
 #grid = urwid.GridFlow(txts, cell_width=20, h_sep=4, v_sep=0, align='left')
 fill = urwid.Filler(cols, 'top')
-loop = urwid.MainLoop(fill, palette, unhandled_input=show_or_exit)
+loop = urwid.MainLoop(fill, unhandled_input=show_or_exit)
 
 loop.run()
 
