23b7880b168f2af975ca977b5c0585ea955f3acd
Paul Ivanov <pi@berkeley.edu>
2014-07-10 15:45:26 -0500

    initial manifest / setup.py stuff

---
+++ setup.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+try:
+    from setuptools import setup
+    have_setuptools = True
+except ImportError:
+    from distutils.core import setup
+    have_setuptools = False
+
+VERSION = "0.5"
+
+setup_kwargs = {
+    "version": VERSION,
+    "description": 'The enlightened pager: less paging. more content. read widely.',
+    "author": 'Paul Ivanov',
+    "author_email": 'pi@berkeley.edu',
+    "url": 'http://kant-en.org/',
+    "download_url": "https://github.com/ivanov/kanten/zipball/" + VERSION,
+    "classifiers": [
+        "License :: OSI Approved",
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+        ],
+    "zip_safe": False,
+    "data_files": [("", ['LICENSE', 'README.md']),],
+    }
+
+if have_setuptools:
+    setup_kwargs['install_requires'] = [
+        'Pygments >= 1.6',
+        'urwid >= 1.1.1',
+        ]
+
+if __name__ == '__main__':
+    setup(
+        name='kanten',
+        py_modules=['kanten'],
+        entry_points={'console_scripts': ['kanten = kanten:main',],},
+        long_description=open('README.md').read(),
+        **setup_kwargs
+        )
